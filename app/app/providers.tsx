82ca3e5b166d8a9f344ad1fa6b6e9465f8d32f60
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-09-12 16:20:36 +0200

    fix(app)!: disable system theming
    NOTICE: untested

--- app/app/providers.tsx
+++ app/app/providers.tsx
@@ -12,7 +12,7 @@ export default function Providers({
 }>) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+      <ThemeProvider attribute="class" defaultTheme="dark" disableTransitionOnChange>
         {children}
       </ThemeProvider>
     </QueryClientProvider>
