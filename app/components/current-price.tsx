9b8d5ceb2c8099aba097db00230416f4446bbc3b
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-09-18 11:33:47 +0200

    refactor(app): current price style

--- app/components/current-price.tsx
+++ app/components/current-price.tsx
@@ -2,12 +2,18 @@ import { Skeleton } from './ui/skeleton';
 
 export default function CurrentPrice({ isPending, value }: { isPending: boolean; value: number }) {
   return (
-    <div>
+    <div className="flex items-center gap-6">
       {isPending ? (
-        <Skeleton className="h-[120px] w-[250px]" />
+        <Skeleton className="h-[120px] w-[320px]" />
       ) : (
-        <h1 className="h-[120px] text-center text-9xl font-bold">{value}</h1>
+        <h1 className="inline-block h-[120px] bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 bg-clip-text text-center text-9xl font-bold text-transparent">
+          {new Intl.NumberFormat('en-US', {
+            minimumFractionDigits: 3,
+            maximumFractionDigits: 3,
+          }).format(value)}
+        </h1>
       )}
+      <div className="text-2xl">kWh</div>
     </div>
   );
 }
