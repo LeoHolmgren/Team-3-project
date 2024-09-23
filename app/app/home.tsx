9b48e6be4bbe4add44985e8550e8d21dc6f6b8df
jusikXL <125464973+jusikXL@users.noreply.github.com>
2024-09-23 21:26:00 +0200

    feat: line chart of mocked price history

--- app/app/home.tsx
+++ app/app/home.tsx
@@ -4,12 +4,34 @@ import { useQuery } from '@tanstack/react-query';
 import { RegionSelect, Status, statuses } from '@/components/region-select';
 import CurrentPrice from '@/components/current-price';
 import { useEffect, useState } from 'react';
+import { Chart } from '@/components/chart';
 
 const PRICE_LABEL = {
-  HIGH: <h2 style={{ opacity: "0.85", fontSize: "5em", lineHeight: 1.1, fontWeight: 800 }} className={"inline-block bg-gradient-to-r from-[#cd7a51] to-[#cd5181] bg-clip-text text-transparent"}>HIGH</h2>,
-  NORM: <h2 style={{ opacity: "0.85", fontSize: "5em", lineHeight: 1.1, fontWeight: 800 }} className={"inline-block bg-gradient-to-r from-[#5157cd] to-[#51cdc7] bg-clip-text text-transparent"}>NORM</h2>,
-  LOW: <h2 style={{ opacity: "0.85", fontSize: "5em", lineHeight: 1.1, fontWeight: 800 }} className={"inline-block bg-gradient-to-r from-[#51cd87] to-[#83cd51] bg-clip-text text-transparent"}>LOW</h2>
-}
+  HIGH: (
+    <h2
+      style={{ opacity: '0.85', fontSize: '5em', lineHeight: 1.1, fontWeight: 800 }}
+      className={'inline-block bg-gradient-to-r from-[#cd7a51] to-[#cd5181] bg-clip-text text-transparent'}
+    >
+      HIGH
+    </h2>
+  ),
+  NORM: (
+    <h2
+      style={{ opacity: '0.85', fontSize: '5em', lineHeight: 1.1, fontWeight: 800 }}
+      className={'inline-block bg-gradient-to-r from-[#5157cd] to-[#51cdc7] bg-clip-text text-transparent'}
+    >
+      NORM
+    </h2>
+  ),
+  LOW: (
+    <h2
+      style={{ opacity: '0.85', fontSize: '5em', lineHeight: 1.1, fontWeight: 800 }}
+      className={'inline-block bg-gradient-to-r from-[#51cd87] to-[#83cd51] bg-clip-text text-transparent'}
+    >
+      LOW
+    </h2>
+  ),
+};
 
 export default function Home() {
   const [selectedZone, setSelectedZone] = useState<Status>(statuses[0]);
@@ -51,11 +73,9 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-6">
-        
-        <CurrentPrice isPending={isPending} value={data ?? 0} property='Price' label={PRICE_LABEL.NORM} />
+        <CurrentPrice isPending={isPending} value={data ?? 0} property="Price" label={PRICE_LABEL.NORM} />
         <RegionSelect selectedZone={selectedZone} setSelectedZone={setSelectedZone} />
-        {/* <Chart /> */}
-
+        <Chart zone={selectedZone.value} />
       </div>
     </div>
   );
