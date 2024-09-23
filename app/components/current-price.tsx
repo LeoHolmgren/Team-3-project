5359e3a240604ad721a656f960e812376dc8f388
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-09-23 13:28:48 +0200

    feat(app): added property-label and value-label to the CurrentPrice component

--- app/components/current-price.tsx
+++ app/components/current-price.tsx
@@ -1,19 +1,22 @@
+import { ReactElement } from 'react';
 import { Skeleton } from './ui/skeleton';
 
-export default function CurrentPrice({ isPending, value }: { isPending: boolean; value: number }) {
-  return (
-    <div style={{alignItems: "baseline"}} className="flex items-center gap-0">
-      {isPending ? (
-        <Skeleton className="h-[120px] w-[320px]" />
-      ) : (
-        <h1 className="inline-block bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 bg-clip-text text-center text-7xl font-bold text-transparent sm:text-8xl md:text-9xl">
-          {new Intl.NumberFormat('en-US', {
-            minimumFractionDigits: 3,
-            maximumFractionDigits: 3,
-          }).format(value)}
-        </h1>
-      )}
-      <p className="text-2xl">SEK / kWh</p>
-    </div>
-  );
+// Container with a property, label & value
+export default function CurrentPrice({ isPending, value, property, label, ...objs }: { isPending: boolean; value: number, property: string, label: ReactElement }) {
+  return <div className="flex flex-col items-center gap-0" {...objs} >
+    {isPending ? (
+      <Skeleton className="h-[10em] w-[18em]" />
+    ) : (
+      <>
+        <h1 className="inline-block opacity-85 text-[1.7em] leading-[1.1] font-[100]">{property}</h1>
+        {label}
+        <h3 className="inline-block opacity-65 text-center text-[2em] leading-[1.1] font-[600] tracking-[-.4px]">
+          {new Intl.NumberFormat('en-US', {maximumSignificantDigits: 3}).format(value)}
+          &nbsp;
+          <span className="text-[0.5em] font-[100]">SEK / kWh</span>
+        </h3>
+      </>
+    )}
+  </div>
+
 }
