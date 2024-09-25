434337dbd7346b1dc5d624a6a4b26b0f3a529a39
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-09-25 20:14:02 +0200

    feat(app): Labels that cange depending on price and location input (#5)
    * feat(app): added location button to and styled region select
    
    * style(app): Styling of region select
    
    * feat(app): region and location highlight on fetched
    
    * fix(app): tailwind ambiguity where selected border color not necessarily prioritized over normal border color
    
    * fix(app): select zone broken to accomodate search on label instead of zone, fix by finding BiddingZone based on label instead
    
    * refactor(app): Made controller interface for region select component as interaction with it was getting quite complex and required multiple properties in single state
    
    * fix(app): useQuery and setZone was having issues where useQuery got stale state. Refactored to use combined state with a controller interface
    
    * feat(app): Fix controller state not changing correctly, now load location and load bidding zone work visually
    
    * feat(app): Ask user for location and load (for now hardcoded) location
    
    * refactor(app): Applied prettier
    
    * feat(app): Change label based on price
    
    * fix(app): Somewhat proper request error handling
    
    * fix(app): Removed intentional error test
    
    * fix(app): Feedback, show se1 by default
    
    * fix(app): Non used Skeleton remove

--- app/components/region-select.tsx
+++ app/components/region-select.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import * as React from 'react';
+import { MutableRefObject, useState, useRef } from 'react';
 
 import { useMediaQuery } from '@/hooks/use-media-query';
 import { Button } from '@/components/ui/button';
@@ -8,99 +8,209 @@ import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, Command
 import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
-export type Status = {
+import { FaLocationDot } from 'react-icons/fa6';
+
+// Bidding zone type
+export type BiddingZone = {
   value: string;
   label: string;
 };
 
-export const statuses: Status[] = [
+// State for the region select interface
+type RegionSelectState = {
+  location_loading: boolean;
+  location_loaded: boolean;
+  zone_loaded: boolean;
+};
+
+// Interface for controlling the Region Controller Visuals
+export interface RegionSelectController {
+  state: RegionSelectState;
+  setLocationLoading: (is_loading: boolean) => void;
+  setLocationLoaded: (is_loaded: boolean) => void;
+  setRegionLoaded: (is_loaded: boolean) => void;
+  setBiddingZoneNoLocation: (zone: BiddingZone) => void;
+}
+
+export const ZONES: BiddingZone[] = [
   {
     value: 'SE1',
-    label: 'SE1: Luleå / Norra Sverige',
+    label: 'North Sweden',
   },
   {
     value: 'SE2',
-    label: 'SE2: Sundsvall / Norra Mellansverige',
+    label: 'North Central Sweden',
   },
   {
     value: 'SE3',
-    label: 'SE3: Stockholm / Södra Mellansverige',
+    label: 'South Central Sweden',
   },
   {
     value: 'SE4',
-    label: 'SE4: Malmö / Södra Sverige ',
+    label: 'South Sweden',
   },
 ];
 
 export function RegionSelect({
   selectedZone,
-  setSelectedZone,
+  loadZone,
+  controllerRef,
 }: {
-  selectedZone: Status;
-  setSelectedZone: (status: Status) => void;
+  selectedZone: BiddingZone | null;
+  loadZone: (zone: BiddingZone) => void;
+  controllerRef: MutableRefObject<RegionSelectController | null>;
 }) {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const isDesktop = useMediaQuery('(min-width: 768px)');
 
-  if (isDesktop) {
-    return (
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button variant="outline" className="w-full justify-center">
-            {selectedZone ? <>{selectedZone.label}</> : <>Select Zone</>}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="p-0" align="center">
-          <RegionList setOpen={setOpen} setSelectedZone={setSelectedZone} />
-        </PopoverContent>
-      </Popover>
-    );
+  const [controllerState, setControllerState] = useState<RegionSelectState>({
+    location_loading: false,
+    location_loaded: false,
+    zone_loaded: false,
+  });
+
+  // Define controller methods that alter controller state for this region select
+  const controller = useRef<RegionSelectController>({
+    state: controllerState,
+    setLocationLoading: (is_loading: boolean) => {
+      controller.current.state = {
+        ...controller.current.state,
+        location_loading: is_loading,
+        location_loaded: false,
+        zone_loaded: false,
+      };
+      setControllerState(controller.current.state);
+    },
+    setLocationLoaded: (is_loaded: boolean) => {
+      controller.current.state = { ...controller.current.state, location_loaded: is_loaded };
+      setControllerState(controller.current.state);
+    },
+    setRegionLoaded: (is_loaded: boolean) => {
+      controller.current.state = { ...controller.current.state, zone_loaded: is_loaded };
+      setControllerState(controller.current.state);
+    },
+    setBiddingZoneNoLocation: (zone: BiddingZone) => {
+      controller.current.state = { ...controller.current.state, location_loading: false, location_loaded: false };
+      setControllerState(controller.current.state);
+      loadZone(zone);
+    },
+  });
+
+  // Pass controller reference
+  controllerRef.current = controller.current;
+
+  let dropdown;
+
+  const dropdown_btn = (
+    <div className="h-[3.5em] w-full">
+      <div
+        className={
+          'flex h-full w-full cursor-pointer items-center justify-between whitespace-nowrap rounded-md border border-input bg-background p-[0.5em] text-[1em] text-sm font-medium leading-[1] text-[#a3a3a3] shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50' +
+          (controllerState.zone_loaded ? ' !border-[#5164cd]' : '')
+        }
+      >
+        <div className="font-[600] text-[#555]">{selectedZone ? selectedZone.value : 'ZONE'}</div>
+        {selectedZone ? selectedZone.label : 'Select Zone'}
+        <div className="font-[600] text-[#555]">20:00</div>
+      </div>
+    </div>
+  );
+
+  async function location_enable() {
+    controller.current.setLocationLoading(true);
+
+    function delay(ms: number) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        async (pos) => {
+          console.log('pos', pos.coords.latitude, pos.coords.longitude);
+          await delay(500);
+          const zone: BiddingZone = ZONES[0]; // TODO: Call our api to get zone from pos.lat, pos.lon
+          controller.current.setLocationLoaded(true);
+          loadZone(zone);
+        },
+        (err) => {
+          console.log('get location error', err);
+          controller.current.setLocationLoading(false);
+        }
+      );
+    } else {
+      controller.current.setLocationLoading(false);
+    }
   }
 
-  return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline" className="w-full justify-center">
-          {selectedZone ? <>{selectedZone.label}</> : <>Select Zone</>}
-        </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="mt-4 border-t">
-          <RegionList setOpen={setOpen} setSelectedZone={setSelectedZone} />
-        </div>
-      </DrawerContent>
-    </Drawer>
+  const location = (
+    <div className="aspect-square h-[3.5em]">
+      <Button
+        variant="outline"
+        onClick={location_enable}
+        className={
+          'h-full w-full justify-center p-[0.5em] text-[1.1em] leading-[1] text-[#555]' +
+          (controllerState.location_loading ? ' !hover:text-[#5164cd] !text-[#5164cd]' : '') +
+          (controllerState.location_loaded ? ' !border-[#5164cd]' : '')
+        }
+      >
+        <FaLocationDot />
+      </Button>
+    </div>
   );
-}
 
-function RegionList({
-  setOpen,
-  setSelectedZone,
-}: {
-  setOpen: (open: boolean) => void;
-  setSelectedZone: (status: Status) => void;
-}) {
-
-  return (
+  const region_list = (
     <Command>
       <CommandInput placeholder="Filter zone..." />
       <CommandList>
         <CommandEmpty>No results found.</CommandEmpty>
         <CommandGroup>
-          {statuses.map((status) => (
+          {ZONES.map((zone) => (
             <CommandItem
-              key={status.value}
-              value={status.value}
+              key={zone.value}
+              value={zone.label}
               onSelect={(value) => {
-                setSelectedZone(statuses.find((priority) => priority.value === value) || statuses[0]);
+                controller.current.setBiddingZoneNoLocation(
+                  ZONES.find((priority) => priority.label === value) || ZONES[0]
+                );
                 setOpen(false);
               }}
             >
-              {status.label}
+              <div>
+                <span className="font-[600] text-[#555]">{zone.value}</span>
+                &nbsp;
+                {zone.label}
+              </div>
             </CommandItem>
           ))}
         </CommandGroup>
       </CommandList>
     </Command>
+  );
+
+  if (isDesktop) {
+    dropdown = (
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>{dropdown_btn}</PopoverTrigger>
+        <PopoverContent className="p-0" align="center">
+          {region_list}
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    dropdown = (
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>{dropdown_btn}</DrawerTrigger>
+        <DrawerContent>
+          <div className="mt-4 border-t">{region_list}</div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return (
+    <div className="flex w-[100%] max-w-[406px] gap-[5px] text-[14px]">
+      {location}
+      {dropdown}
+    </div>
   );
 }
