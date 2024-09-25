e4b429c917804167bd0833b0374e0a98959e1f27
LeoHolmgren <78541527+LeoHolmgren@users.noreply.github.com>
2024-09-25 15:52:27 +0200

    "use client" added to footer.tsx

--- app/app/footer.tsx
+++ app/app/footer.tsx
@@ -1,18 +1,18 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 
 export default function Footer() {
   const [timestamp, setTimestamp] = useState<string>('');
 
   useEffect(() => {
-    // Get the current date and time when the component is loaded
     const currentTimestamp = new Date().toLocaleString();
     setTimestamp(currentTimestamp);
-  }, []); // Empty array ensures this runs only when the component mounts (initial load)
+  }, []);
 
   return (
     <div className="py-8 text-center text-sm text-muted-foreground">
-      <p>Made with ##### ❤️ by Team #3</p>
-      <p>Page loaded on: {timestamp}</p>
+      <p>Made with ❤️ by Team #3</p>
+      <p>Refreshed: {timestamp}</p>
     </div>
   );
 }
