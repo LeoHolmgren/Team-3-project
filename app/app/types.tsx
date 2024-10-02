115717076a0cb245b809b3dc6578822dc7a3d72e
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-10-02 15:47:29 +0200

    feat(app): Show default 'No Zone Selected' screen when no zone is selected and refactoring (#14)
    * feat(app): Show default 'No Zone Selected' screen when no zone is selected
    
    * fix(app): Store No Selected Zone image locally

---
+++ app/app/types.tsx
@@ -0,0 +1,11 @@
+export type PriceData = Array<{ price: number; time: number }>;
+
+export type PriceLevels = {
+  high: number;
+  low: number;
+};
+
+export type BiddingZone = {
+  value: string;
+  label: string;
+};
