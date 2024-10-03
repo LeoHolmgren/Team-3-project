dcddbfef458b992efadb08423df6fd3b4d006b7d
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-10-03 15:36:59 +0200

    feat(app): Starting to connect app fetch with our API

--- app/app/api.tsx
+++ app/app/api.tsx
@@ -5,25 +5,12 @@ const bidigit = new Intl.NumberFormat('en-US', { minimumIntegerDigits: 2 });
 // Call external api to get price for zone
 export async function fetchPrice(zone: BiddingZone): Promise<{ arrived: Date; data: PriceData }> {
   
-  //const response = await fetch('http://127.0.0.1:8000/price-data/SE1');
-  //console.log(response);
-  //console.log(await response.text());
-
   const currTime = new Date(Date.now() - 0 * 1000 * 60 * 60 * 24 * 1);
   const year = currTime.getFullYear();
   const month = currTime.getMonth() + 1;
   const day = currTime.getDate();
 
-  const URL =
-    'https://www.elprisetjustnu.se/api/v1/prices/' +
-    year +
-    '/' +
-    bidigit.format(month) +
-    '-' +
-    bidigit.format(day) +
-    '_' +
-    zone.value +
-    '.json';
+  const URL = 'http://127.0.0.1:8000/price-data/date/'+year+'-'+bidigit.format(month)+'-'+bidigit.format(day)+'?price_data_zone=' + zone.value;
 
   return fetch(URL).then(async (response) => {
     if (!response.ok) {
@@ -31,14 +18,11 @@ export async function fetchPrice(zone: BiddingZone): Promise<{ arrived: Date; da
     }
 
     const timeOfData = new Date();
-    const json: Array<{ SEK_per_kWh: number }> = await response.json();
+    const json: {arrived: number, data: Array<{ price: number, time: number }>} = await response.json();
 
     return {
       arrived: timeOfData,
-      // Reformat data to be of type { price: number, time: number }
-      data: json.map((data: { SEK_per_kWh: number }, idx: number) => {
-        return { price: data['SEK_per_kWh'], time: idx };
-      }),
+      data: json.data,
     };
   });
 }
