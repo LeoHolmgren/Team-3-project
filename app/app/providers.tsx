67a0b2920b54b472c595c7082c8e1b513b0531b5
LeoHolmgren <78541527+LeoHolmgren@users.noreply.github.com>
2024-10-11 18:39:56 +0200

    fix(app): providers updated.

--- app/app/providers.tsx
+++ app/app/providers.tsx
@@ -2,18 +2,23 @@
 
 import { ThemeProvider } from '@/components/theme-provider';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AppProvider } from './appContext';
 
 const queryClient = new QueryClient();
 
 export default function Providers({
   children,
+  resetAppState,
 }: Readonly<{
   children: React.ReactNode;
+  resetAppState: () => void;  
 }>) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-        {children}
+        <AppProvider resetAppState={resetAppState}>
+          {children}
+        </AppProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
