b81ea963bdee8a782d980784c931714c6faab9f5
LeoHolmgren <78541527+LeoHolmgren@users.noreply.github.com>
2024-10-11 15:38:36 +0200

    fix(app): padding added to home.

--- app/app/home.tsx
+++ app/app/home.tsx
@@ -133,7 +133,7 @@ export default function Home() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center gap-6">
+    <div className="flex flex-col items-center justify-center gap-6 pt-16">
       <Header onClickLogo={resetAppState} />
       <ContentPanel state={homeState}></ContentPanel>
       <RegionSelect
