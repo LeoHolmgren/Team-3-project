e5159b7e73b024faa5abf63f8dce817709bec357
Anton Kling <anton@kling.gg>
2024-10-14 16:07:40 +0000

    Fix previous incorrect merge (#35)
    fix(API): Fixes mistake made in previous merge

--- api/routes.py
+++ api/routes.py
@@ -87,28 +87,6 @@ async def create_price_data(zone: str, price_sek: Decimal, time_start: datetime,
     "created_at": new_price_data.created_at
     }
 
-# GET endpoint: Fetch data by ID
-@router.get("/price-data/{price_data_id}")
-async def read_price_data(price_data_id: int, db: Session = Depends(get_db)):
-
-# Fetches a specific price data entry from the electricity_prices table (SQL)
-
-    query = text("SELECT * FROM price_data WHERE id = :id")
-    result = db.execute(query, {"id": price_data_id})
-    price_data = result.fetchone()
-
-    if price_data is None:
-        raise HTTPException(status_code=404, detail="PriceData not found")
-
-    return {
-    "id": price_data.id,
-    "zone": price_data.zone,
-    "price_sek": price_data.price_sek,  # Ensure case matches the DB
-    "time_start": price_data.time_start,
-    "time_end": price_data.time_end,
-    "created_at": price_data.created_at  # Include created_at for completeness
-    }
-
 # GET endpoint: Fetch data by Zone
 @router.get("/price-data/{price_data_zone}")
 async def read_price_data_zone(price_data_zone: str, db: Session = Depends(get_db)):
