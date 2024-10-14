9a2e90b05c5d4e8fc66e0812de96bf22ee992501
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-10-14 19:20:54 +0200

    refactor(app): Location Zone Button Component

--- app/app/constants.tsx
+++ app/app/constants.tsx
@@ -1,1 +1,29 @@
+import { PriceLevels, BiddingZone } from '@/app/types';
+
 export const STORE_HISTORY_COOKIE = 'BiddingZone';
+
+export const MOCK_PRICE_LEVELS: PriceLevels = {
+  high: 0.2,
+  low: 0.1,
+};
+
+export const ZONES: BiddingZone[] = [
+  {
+    value: 'SE1',
+    label: 'North Sweden',
+  },
+  {
+    value: 'SE2',
+    label: 'North Central Sweden',
+  },
+  {
+    value: 'SE3',
+    label: 'South Central Sweden',
+  },
+  {
+    value: 'SE4',
+    label: 'South Sweden',
+  },
+];
+
+export const BIDIGIT: Intl.NumberFormat = new Intl.NumberFormat('en-US', { minimumIntegerDigits: 2 });
