739a9cc2337d93c00cbdff5cf8a68485eb7d1c3a
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-10-14 16:38:02 +0200

    feat(app): Use cookie for store history

--- app/app/home.tsx
+++ app/app/home.tsx
@@ -3,13 +3,13 @@
 import { PriceData, PriceLevels, BiddingZone } from '@/app/types';
 import ContentPanel from '@/components/content-panel';
 import { RegionSelect, RegionSelectController } from '@/components/region-select';
-import { useState, useRef, useLayoutEffect } from 'react';
+import { useState, useRef } from 'react';
 import Header from './header';
 import Footer from './footer';
 import fetchPrice from '@/app/api';
-import { getStoredBiddingZone, setStoredBiddingZone } from '@/app/local-storage';
-import { Skeleton } from '@/components/ui/skeleton';
 import { AppProvider } from './appContext';
+import useCookie from '@/hooks/use-cookie';
+import { STORE_HISTORY_COOKIE } from '@/app/constants';
 
 export type HomeState = {
   zone: BiddingZone | null;
@@ -32,12 +32,13 @@ const MOCK_PRICE_LEVELS: PriceLevels = {
   low: 0.1,
 };
 
-export default function Home() {
-  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+export default function Home({ loadZone }: { loadZone: BiddingZone | null }) {
+  const loaded = useRef<boolean>(false);
   const regionSelectControllerRef = useRef<RegionSelectController>(null);
+  const [, setZoneCookie] = useCookie<BiddingZone | null>(STORE_HISTORY_COOKIE, null);
 
   const [homeState, setHomeState] = useState<HomeState>({
-    zone: null,
+    zone: loadZone,
     isFetchingPrice: false,
     timeOfFetch: null,
     fetchData: null,
@@ -46,7 +47,7 @@ export default function Home() {
     error: null,
   });
 
-   // Reset app state when the logo is clicked
+  // Reset app state when the logo is clicked
   const resetAppState = () => {
     setHomeState({
       zone: null,
@@ -84,7 +85,7 @@ export default function Home() {
       // Price starts loading, update state
       regionSelectControllerRef.current?.setRegionLoaded(false);
       setHomeState(homeController.current.state);
-      setStoredBiddingZone(zone);
+      setZoneCookie(zone, { expires: 30 }); // Cookie expires in 30 days
 
       let response;
 
@@ -116,35 +117,24 @@ export default function Home() {
     },
   });
 
-  useLayoutEffect(() => {
-    if (!isLoaded) {
-      const biddingZone: BiddingZone | null = getStoredBiddingZone();
-      if (biddingZone) homeController.current.loadBiddingZone(biddingZone);
-      setIsLoaded(true);
-    }
-  });
-
-  if (!isLoaded) {
-    return (
-      <div className="flex flex-col items-center justify-center">
-        <Skeleton className="h-[29.625em] w-[25.375em]" />
-        <Footer timestamp={homeState.timeOfFetch} />
-      </div>
-    );
+  // On first load, if there is a loadZone, load it
+  if (!loaded.current) {
+    if (loadZone) homeController.current.loadBiddingZone(loadZone);
+    loaded.current = true;
   }
 
   return (
     <AppProvider resetAppState={resetAppState}>
       <Header />
-        <div className="flex flex-col items-center justify-center gap-6 pt-24">
-          <ContentPanel state={homeState}></ContentPanel>
-          <RegionSelect
-            state={homeState}
-            homeController={homeController.current}
-            controllerRef={regionSelectControllerRef}
-          />
-          <Footer timestamp={homeState.timeOfFetch} />
-        </div>
+      <div className="flex flex-col items-center justify-center gap-6 pt-24">
+        <ContentPanel state={homeState}></ContentPanel>
+        <RegionSelect
+          state={homeState}
+          homeController={homeController.current}
+          controllerRef={regionSelectControllerRef}
+        />
+        <Footer timestamp={homeState.timeOfFetch} />
+      </div>
     </AppProvider>
   );
 }
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from 'next';
 import HomeComponent from '@/app/home';
+import { cookies } from 'next/headers';
+import { BiddingZone } from '@/app/types';
+import { STORE_HISTORY_COOKIE } from '@/app/constants';
+
+function getZoneCookie(): BiddingZone | null {
+  const cookie = cookies().get(STORE_HISTORY_COOKIE);
+  if (cookie) {
+    return JSON.parse(cookie.value) as BiddingZone;
+  }
+  return null;
+}
 
 export const metadata: Metadata = {
   title: 'OnOff',
@@ -7,5 +18,5 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  return <HomeComponent />;
+  return <HomeComponent loadZone={getZoneCookie()} />;
 }
