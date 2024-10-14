259460c11adf49769f9c1b891310a931ce403b54
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-10-14 15:41:22 +0200

    fix(app): Fix cookie type definitions

--- app/hooks/use-cookie.tsx
+++ app/hooks/use-cookie.tsx
@@ -1,27 +1,29 @@
 import { useState, useCallback } from "react"
 import Cookies from "js-cookie"
 
-export default function useCookie<T>(name: string, defaultValue: T) {
-    
-  const [value, setValue] = useState(() => {
+export default function useCookie<T>(name: string, defaultValue: T): [T, (newValue: T, options?: Cookies.CookieAttributes) => void, () => void] {
+
+  function getCookie(): T {
     const cookie = Cookies.get(name)
-    if (cookie) return cookie
+    if (cookie) return (JSON.parse(cookie) as T)
     Cookies.set(name, JSON.stringify(defaultValue))
     return defaultValue
-  })
+  }
 
-  const updateCookie = useCallback(
-    (newValue: T, options: Cookies.CookieAttributes) => {
+  const [cookie, updateCookieState] = useState<T>(getCookie())
+
+  const setCookie = useCallback(
+    (newValue: T, options?: Cookies.CookieAttributes) => {
       Cookies.set(name, JSON.stringify(newValue), options)
-      setValue(newValue)
+      updateCookieState(newValue)
     },
     [name]
   )
 
   const deleteCookie = useCallback(() => {
     Cookies.remove(name)
-    setValue(defaultValue)
+    updateCookieState(defaultValue)
   }, [name, defaultValue])
 
-  return [value, updateCookie, deleteCookie]
+  return [cookie, setCookie, deleteCookie]
 }
