b082e3307c4cebbd7498e21dd437cc0454f54e7a
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-10-16 18:13:03 +0200

    feat(scripts): Unix  - Added auto creation of config.env on setup and using vars in that file for run scripts

--- scripts/run-aggregator.sh
+++ scripts/run-aggregator.sh
@@ -8,6 +8,7 @@ cd $ROOTDIR/aggregator
 
 # Run aggregator
 source .venv/bin/activate
+source $ROOTDIR/scripts/set-env.sh
 export PYTHONPATH=$ROOTDIR
 python3 api.py
 deactivate
--- scripts/run-api.sh
+++ scripts/run-api.sh
@@ -8,6 +8,7 @@ cd $ROOTDIR/api
 
 # Run API
 source .venv/bin/activate
+source $ROOTDIR/scripts/set-env.sh
 export PYTHONPATH=$ROOTDIR
 fastapi dev main.py
 deactivate
