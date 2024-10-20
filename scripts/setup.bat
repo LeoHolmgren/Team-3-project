34b485cb57bb5574024f18e4bd98c2301eb21ed9
Kevin <28414525+Kevsterking@users.noreply.github.com>
2024-10-20 18:05:03 +0200

    fix: scripts py changed to python

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -9,21 +9,21 @@ echo No|copy /-Y %~dp0\TEMPLATE.env %~dp0..\config.env
 :: Install aggregator environment
 echo Setting up aggregator environment...
 cd %~dp0..\aggregator
-py -3 -m venv .venv
+python -m venv .venv
 echo Installing aggregator dependencies...
 .\.venv\Scripts\pip3.exe install -r ./requirements.txt
 
 :: Install API environment
 echo Setting up API environment...
 cd %~dp0..\api
-py -3 -m venv .venv
+python -m venv .venv
 echo Installing API dependencies...
 .\.venv\Scripts\pip3.exe install -r ./requirements.txt
 
 :: Install test environment
 echo Setting up the testing environment...
 cd %~dp0..\tests
-py -3 -m venv .venv
+python -m venv .venv
 echo Installing test dependencies...
 .\.venv\Scripts\pip3.exe install -r ./requirements.txt
 
